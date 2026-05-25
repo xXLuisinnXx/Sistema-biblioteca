c2b631b2ca7692716777961607875dd7abf0e5a1
xXLuisinnXx <carvalhovaladaresluisguilherme@gmail.com>
2026-05-25 16:04:53 -0300

    Tarefa: configuração inicial do projeto com dependecias e configuração do H2

---
+++ src/test/java/com/sistema_biblioteca/app/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema_biblioteca.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
