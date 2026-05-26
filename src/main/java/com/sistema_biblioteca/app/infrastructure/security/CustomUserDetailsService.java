d71ac1ad497767db5fd056b578f0b09ff34d1b0b
xXLuisinnXx <carvalhovaladaresluisguilherme@gmail.com>
2026-05-26 18:32:04 -0300

    Tarefa: adiciona CustomUserDetailsService para autenticacao via banco de dados

---
+++ src/main/java/com/sistema_biblioteca/app/infrastructure/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.sistema_biblioteca.app.infrastructure.security;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.sistema_biblioteca.app.domain.model.Usuario;
+import com.sistema_biblioteca.app.domain.repository.UsuarioRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    // Implementação do serviço de detalhes do usuário
+    private final UsuarioRepository userRepository;
+
+    public CustomUserDetailsService(UsuarioRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // Lógica para carregar o usuário pelo email
+        Usuario usuario = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com email: " + email));
+
+        return User.builder()
+                .username(usuario.getEmail())
+                .password(usuario.getSenha())
+                .roles(usuario.getPerfil()) 
+                .build();
+    }
+
+
+}
